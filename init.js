670c40c3cb4e0d9a30c229d0a56bd0b69d6d08a0
Marvin Zhang <tikazyq@gmail.com>
2019-09-14 09:36:57 +0800

    added system setting

--- init.js
+++ init.js
@@ -27,8 +27,7 @@ const init = async () => {
     const environment = data.environments[i]
     let environmentDb = await models.Environment.findOne({ _id: environment._id })
     if (!environmentDb) {
-      environmentDb = new models.Environment(environment)
-      await environmentDb.save()
+      // do nothing
     } else {
       for (let key in environment) {
         if (environment.hasOwnProperty(key)) {
