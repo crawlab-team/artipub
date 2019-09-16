dbbeabe1166834d9b372995c2c59f73a979eff07
marvzhang <tikazyq@163.com>
2019-09-16 13:27:49 +0800

    finalized oschina spider

--- spiders/oschina.js
+++ spiders/oschina.js
@@ -101,6 +101,29 @@ class OschinaSpider extends BaseSpider {
   }
 
   async fetchStats() {
+    if (!this.task.url) return
+    await this.page.goto(this.task.url, { timeout: 60000 })
+    await this.page.waitFor(5000)
+
+    const stats = await this.page.evaluate(() => {
+      const text = document.querySelector('body').innerText
+      const mRead = text.match(/阅读 (\d+)/)
+      const mLike = text.match(/点赞 (\d+)/)
+      const mComment = text.match(/评论 (\d+)/)
+      const readNum = mRead ? Number(mRead[1]) : 0
+      const likeNum = mLike ? Number(mLike[1]) : 0
+      const commentNum = mComment ? Number(mComment[1]) : 0
+      return {
+        readNum,
+        likeNum,
+        commentNum
+      }
+    })
+    this.task.readNum = stats.readNum
+    this.task.likeNum = stats.likeNum
+    this.task.commentNum = stats.commentNum
+    await this.task.save()
+    await this.page.waitFor(3000)
   }
 }
 
