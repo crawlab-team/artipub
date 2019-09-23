bb18dab755b5a8872832231f1cc4d1f9571f63ff
marvzhang <tikazyq@163.com>
2019-09-23 22:10:03 +0800

    fixed mongodb missing authenticationDatabase parameter issue

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    HOST: '0.0.0.0',
-    PORT: 3000,
-    MONGO_HOST: 'localhost',
-    MONGO_PORT: '27017',
-    MONGO_DB: 'artipub',
-    MONGO_USERNAME: '',
-    MONGO_PASSWORD: '',
+  HOST: '0.0.0.0',
+  PORT: 3000,
+  MONGO_HOST: 'localhost',
+  MONGO_PORT: '27017',
+  MONGO_DB: 'artipub',
+  MONGO_USERNAME: '',
+  MONGO_PASSWORD: '',
+  MONGO_AUTH_DB: 'admin'
 }
