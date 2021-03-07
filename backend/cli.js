da80ff4c476a42ac83598954b54ac577ef221b34
泡泡机不冒泡了 <undefined4me@gmail.com>
2021-03-07 19:10:03 +0800

    release

--- backend/cli.js
+++ backend/cli.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-const { version }= require('./package.json');
 const  program = require('commander')
 
 program
-  .version(version)
   .command('start')
   .description('Start ArtiPub backend server')
   .option('-H, --host <host>', 'mongodb host name', '127.0.0.1')
