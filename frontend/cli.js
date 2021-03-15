4fbeb3d5cf07cc30241959434f2d12227dd20e9f
泡泡机不冒泡了 <undefined4me@gmail.com>
2021-03-15 12:50:59 +0800

    修复前端启动脚本配置服务端地址

--- frontend/cli.js
+++ frontend/cli.js
@@ -2,7 +2,10 @@
 const { version }= require('./package.json');
 const path = require('path');
 const exec = require('child_process').exec
-const  program = require('commander')
+const program = require('commander');
+const replace = require('replace-in-file');
+const { exit } = require('process');
+const defaultBackendHost = 'http://localhost:3000';
 
 const distDir = path.join(
   __dirname, './dist'
@@ -12,10 +15,28 @@ program
   .version(version)
   .command('start')
   .description('Start ArtiPub frontend server')
-  .option('-h, --host <host>', 'host name', '127.0.0.1')
-  .option('-p, --port <port>', 'port number', 8000)
+  .option('-h, --backendHost <host>', `backend server address, default: ${defaultBackendHost}`, defaultBackendHost)
+  .option('-p, --port <port>', 'frontend static server port number', 8000)
   .action((options) => {
-    const setUpCmd = `npx http-server ${distDir} -a ${options.host} -p ${options.port}`;
+    //非默认就替换前端脚本中后端地址
+    if (options.backendHost !== defaultBackendHost) {
+      const umiIndex = path.join(distDir, './umi.*.js');
+      const replaceOption = {
+        files: umiIndex,
+        from: defaultBackendHost,
+        to: options.backendHost
+      }
+      
+      try {
+        replace.sync(replaceOption);
+      } catch(error) {
+        console.error(`替换 ${umiIndex} 脚本中后端地址失败，请确保有修改权限。`);
+        exit(1);
+      }
+
+    }
+
+    const setUpCmd = `npx http-server ${distDir} -p ${options.port}`;
 
     // 开启前端服务
     console.log(`启动命令：${setUpCmd}`)
